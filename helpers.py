73d041e208eb36213c349fc024ca979013a97315
chadji15 <chadji15@cs.ucy.ac.cy>
2021-07-28 18:57:18 +0300

    fix git

--- helpers.py
+++ helpers.py
@@ -2,14 +2,16 @@ import pickle
 from eval import eval_one_to_one_3x3
 from neat import Config, DefaultReproduction, DefaultSpeciesSet, DefaultStagnation
 from solve import convert_to_action2, convert_to_action3, convert_to_action4, convert_to_direction
-from switch_neat import Agent, create
+from switch_neat import Agent
+import switch_neat
+import switch_maps
 from deap import creator, base
-from deap_neat_old import DeapSwitchGenome
+from deap_neat import DeapSwitchGenome, DeapSwitchMapGenome
 
-def get_grid():
+def get_grid(resfile = 'final.p'):
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", DeapSwitchGenome, fitness=creator.FitnessMax, features = list)
-    res = pickle.load(open("final.p", "rb"))
+    res = pickle.load(open(resfile, "rb"))
     grid = res['container']
     return grid
 
@@ -29,13 +31,13 @@ def get_agent_onf(size, config_file):
     agent = Agent(net, lambda x: x, outf)
     return agent
 
-def get_best_agent(size, config_file):
-    grid = get_grid()
+def get_best_agent(size, config_file, resfile='final.p'):
+    grid = get_grid(resfile)
     ind = grid.best
-    conf = Config(DeapSwitchGenome, DefaultReproduction,
+    conf = Config(DeapSwitchMapGenome, DefaultReproduction,
                   DefaultSpeciesSet, DefaultStagnation,
                   config_file)
-    net = create(ind, conf)
+    net = switch_maps.create(ind, conf, 3)
     if size == 2:
         outf = convert_to_action2
     elif size == 3:
@@ -56,3 +58,14 @@ def get_best_tmaze():
     agent = Agent(net, lambda x: x, outf)
     return agent
 
+def verify_best_agent():
+    agent = get_best_agent(3, 'config/deap-maps-skinner3','out/3x3_qd_maps/skinner3_final.p')
+    score, bd = eval_one_to_one_3x3(agent, 200,40,20,True)
+    print(f"score: {score}, bd: {bd}")
+    scores = [eval_one_to_one_3x3(agent,200,40) for _ in range(1000)]
+    scores.sort()
+    print(f"scores: {scores}")
+    print(f"{len(list(filter( lambda x: x < 170, scores)))} scores are below 170")
+
+if __name__ == '__main__':
+    verify_best_agent()
